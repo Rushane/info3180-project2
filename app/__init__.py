fd52ba8d63d03249f6e6b9b4e4171a3dfa22d8fd
Lanai Nevers <lanai.nevers@mymona.uwi.edu>
2019-05-03 06:55:45 +0000

    Changed Database URI

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:project2@localhost/project2"
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://qfeyffwhdwnxgz:a59656f8d9daab3e461a7fa7096730c53969f7432e3fb0b80e5630f73d791c75@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d2o6e4huklcd45"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:project2@localhost/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://qfeyffwhdwnxgz:a59656f8d9daab3e461a7fa7096730c53969f7432e3fb0b80e5630f73d791c75@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d2o6e4huklcd45"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 PROFILE_IMG_UPLOAD_FOLDER = os.path.join("static/uploads", "profile_photos")
